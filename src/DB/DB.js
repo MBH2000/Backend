894be6276457bfa8297e48147baa544d79ec7218
najeeb bh <ava201466@gmail.com>
2023-06-16 08:49:18 -0400

    s

--- src/DB/DB.js
+++ src/DB/DB.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.9.0',{
+mongoose.connect('mongodb+srv://MBH:Avatarpower2012@app.hsv9n.mongodb.net/?retryWrites=true&w=majority',{
     useNewUrlParser : true
 })
 //mongodb+srv://MBH:petstore2022@app.hsv9n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
